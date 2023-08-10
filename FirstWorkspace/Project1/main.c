e6d9572e91163e319e3ff2b7f5a0a3ca7d41c19d
Zeshan Rasul <zeshanrasul@gmail.com>
2023-08-10 18:59:57 +0100

    Favourite number program

---
+++ FirstWorkspace/Project1/main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main() {
+    int favouriteNumber = 0;
+    printf("What is your favourite number?\n");
+    scanf("%d/n", &favouriteNumber);
+    printf("Your favourite number is: %d\n", favouriteNumber);
+}
